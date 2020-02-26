3189e8bdf2696cb62b90067064555ca37f04533c
kingbabar8123 <54685065+kingbabar8123@users.noreply.github.com>
2020-02-25 20:44:38 -0600

    Update script.js
    updated java script to test for special characters

--- js/script.js
+++ js/script.js
@@ -2,14 +2,22 @@ function init(){
 //add your javascrip between these two lines of code
 
 document.getElementById('button').addEventListener('click', function(){
-	if (document.getElementById('password').value) != document.getElementByClass('float').value)) {
-		alert('Your passwords do not match, please try again.');
-	}
-	else {
-	alert('you did it');
-}
-	document.getElementById('textoutput').innerHTML = document.getElementById('entryinput').value;
-};
+	var pw = document.getElementById('password').value;
+	var pw1 = document.getElementById('input_float_pw').value;
+	if ( pw != pw1) {
+		alert('Your passwords dont match please try again');
+	}else {
+		var regex = /^[A-Za-z0-9 ]+$/
+        var isValid = regex.test(pw);
+        if (isValid) {
+            alert("Your password must contain at least one special character to proceed");
+        } else {
+            alert("Password passes requirements");
+        }
+ 
+        return isValid;
+    }
+});
 
 }
  
