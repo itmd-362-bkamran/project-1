ee6f23af1528304952fc3d65bc5d306a35c8b5ba
kingbabar8123 <54685065+kingbabar8123@users.noreply.github.com>
2020-02-25 20:17:09 -0600

    class to id
    changed some elements from class to id

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@ function init(){
 //add your javascrip between these two lines of code
 
 document.getElementById('button').addEventListener('click', function(){
-	alert('Babar Kamran: ' + document.getElementById('password').value);
+	if (document.getElementById('password').value) != document.getElementByClass('float').value)) {
+		alert('Your passwords do not match, please try again.');
+	}
+	else {
+	alert('you did it');
+}
 	document.getElementById('textoutput').innerHTML = document.getElementById('entryinput').value;
-});
+};
 
 }
  
