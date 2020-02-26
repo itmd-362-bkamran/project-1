3875a368bf213e7e39cf22d8e78c557109f97746
kingbabar8123 <54685065+kingbabar8123@users.noreply.github.com>
2020-02-25 21:51:06 -0600

    clean up

--- js/script.js
+++ js/script.js
@@ -4,20 +4,25 @@ function init(){
 document.getElementById('button').addEventListener('click', function(){
 	var pw = document.getElementById('password').value;
 	var pw1 = document.getElementById('input_float_pw').value;
+	var email = document.getElementById('email').value;
+	var regex = /^[A-Za-z0-9 ]+$/;
 	if ( pw != pw1) {
 		alert('Your passwords dont match please try again');
 	}else {
-		var regex = /^[A-Za-z0-9 ]+$/
         var isValid = regex.test(pw);
         if (isValid) {
             alert("Your password must contain at least one special character to proceed");
         } else {
             alert("Password passes requirements");
-			document.getElementById("myform").reset()
+			
         }
- 
-        return true;
     }
+	if (email != 'a') {
+	alert('test');
+	document.getElementById("myform").submit();
+	}else {
+		document.getElementById("myform").submit();
+	}
 });
 
 }
